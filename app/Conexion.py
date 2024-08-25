ad02f5a19e019c3fd90765c57e9a7c4f1d747a9f
BorisMejia <borisfernandez916@gmail.com>
2024-08-25 16:10:18 -0500

    cambio de db y integracion de docker al proyecto

--- app/Conexion.py
+++ app/Conexion.py
@@ -2,10 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+DATABASE_HOST = "127.0.0.1"
+DATABASE_PORT = "5432"
+DATABASE_NAME = "fastapi"
+DATABASE_USER = "root"
+DATABASE_PASSWORD = "root"
 
-DATABASE_URL = "mysql+mysqlconnector://root:@localhost:3306/fastapi"
+DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
+#DATABASE_URL = "mysql+mysqlconnector://root:@localhost:3306/fastapi"
+#DATABASE_URL = "postgresql://root:root@postgresql:5432/fastapi"
 engine = create_engine(DATABASE_URL)
+#SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
 SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
